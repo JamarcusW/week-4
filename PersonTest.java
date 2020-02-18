296670fb5df03382b3c905af8164d8459940962a
JamarcusW <60477746+JamarcusW@users.noreply.github.com>
2020-02-18 10:38:34 -0500

    week 4 work

---
+++ PersonTest.java
@@ -0,0 +1,27 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PersonTest {
+
+	private Person p1;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		
+		p1 = new Person();
+		
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
